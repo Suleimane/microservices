88af9dc4d15fdd94d4b480078041a0140fe52085
Suleimane <manuel.sbmn@gmail.com>
2022-09-07 22:53:11 -0300

    Project hr-api-gateway-zull created

---
+++ hr-api-gateway-zull/src/test/java/com/suleimane/hrgatewayzull/HrApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suleimane.hrgatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
