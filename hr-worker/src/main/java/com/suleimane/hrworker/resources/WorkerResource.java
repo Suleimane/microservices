ea2241a2ebf3b0d51afea0cba9d672329b77734f
Suleimane <manuel.sbmn@gmail.com>
2022-12-01 16:35:14 -0300

    Zull Authorization

--- hr-worker/src/main/java/com/suleimane/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/suleimane/hrworker/resources/WorkerResource.java
@@ -47,11 +47,11 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> consultar(@PathVariable Long id){
-		try {
-			Thread.sleep(3000L);
-		}catch(InterruptedException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(3000L);
+//		}catch(InterruptedException e) {
+//			e.printStackTrace();
+//		}
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
